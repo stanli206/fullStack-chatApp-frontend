81bc308450f7e562ff5dcfa7c45dea612579a11b
stanli206 <stantilin@gmail.com>
2025-08-15 21:18:19 +0530

    `Update axios instance configuration to include localhost fallback`

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,17 @@
+// import axios from "axios";
+
+// export const axiosInstance = axios.create({
+//   baseURL: "https://fullstack-chat-app-d20c.onrender.com/api",
+//   withCredentials: true,
+// });
+// // http://localhost:5001
+
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://fullstack-chat-app-d20c.onrender.com/api",
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:5001/api"
+      : "https://fullstack-chat-app-d20c.onrender.com/api",
   withCredentials: true,
 });
-// http://localhost:5001
